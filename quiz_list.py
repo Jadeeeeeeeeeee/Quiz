c1c807c80814a817c0c14eae3ed085e02a4ae04f
Jade Ramos <105251276+Jadeeeeeeeeeee@users.noreply.github.com>
2022-07-06 01:00:26 +0000

    Completed adding all the questions

--- quiz_list.py
+++ quiz_list.py
@@ -1,9 +1,7 @@
-#Base quiz learning code
-
+#imports
 import sys, json
 import time
 from time import sleep
-
 #Define a fuction to write words
 def write(words):
   for char in words:
@@ -11,12 +9,11 @@ def write(words):
     sys.stdout.write(char)
     sys.stdout.flush()
 
-#Greeting
+#Instructions and greeting
 write("Welcome to Te Reo quiz, Please enter your name:")
 name = input("")
 print("")
-write("Kia ora!, " + name + "!, Welcome to Te Reo quiz!, This quiz is a multiple choice quiz so please pick one of the letter choices.Have fun and enjoy the quiz!")
-print("")
+write("Kia ora!, " + name + "!, Welcome to Te Reo quiz!, This quiz is a multiple choice quiz so please pick one of the letter choices.Have fun and enjoy the quiz!\n")
 time.sleep(0.5)
 #import random library
 import random  
@@ -24,10 +21,10 @@ import random
 #globals and questions lists
 score = 0
 #global and quesitons lists
-english = ["hello", "welcome", "goodmorning", "egg", "dog", "cat" ]
-right_answer = ["Kia ora", "Nau mai, haere mai mean", "mōrena", "hēki", "pero","ngeru"]
-option_1 = ["pero" , "aroha" , "pero", "ngeru", "koe",""]
-option_2 = ["Nau mai, haere hai" , "mōrena" , "hēki", "Nau mai", "ngeru"]
+english = ["hello", "welcome", "goodmorning", "egg", "dog", "cat", "love","lake","sea","family" ]
+right_answer = ["Kia ora", "Nau mai, haere mai mean", "mōrena", "hēki", "pero","ngeru", "aroha", "awa","moana","whanau"]
+option_1 = ["pero" , "aroha" , "kai ", "ngeru", "koe","aroha","pō mārie","tāne","taonga","awa"]
+option_2 = ["Nau mai, haere hai" , "mōrena" , "hēki", "Nau mai", "ngeru","awa","tama","tamāhine","motu"]
 
 #define a function to generate a question
 def generate_question(english, right_answer, option_1, option_2):
@@ -43,10 +40,11 @@ def generate_question(english, right_answer, option_1, option_2):
     answer = input().lower()
     if answer == "c":
       score += 1
+      print("Correct!")
       print("Score:", score)
     else:
       print("Score:", score)
-      print("incorrect.")
+      print("Incorrect")
 
   elif (random_sequence == 1):
     print("A.", option_1)
@@ -54,6 +52,7 @@ def generate_question(english, right_answer, option_1, option_2):
     print("C.", option_2 ,"\nAnswer:")
     answer = input().lower()
     if answer == "b":
+      print("Correct!")
       score += 1
       print("Score:", score)
     else:
@@ -66,13 +65,14 @@ def generate_question(english, right_answer, option_1, option_2):
     print("C.", option_1 ,"\nAnswer:")
     answer = input().lower()
     if answer == "a":
+      print("Correct!")
       score += 1
       print("Score:", score)
     else:
       print("incorrect.")
       print("Score:", score)
 
-for i in range (0, 5):
+for i in range (0, 10):
   generate_question(english[i],right_answer[i],option_1[i],option_2[i])
 
 def total_score():
